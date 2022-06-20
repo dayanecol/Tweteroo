98e5c7916bdb8d639f02cb7037ffe66cb1b0d67d
Dayane <dayanecarolina.oliveira@gmail.com>
2022-06-20 11:01:42 -0300

    Post tweets done

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ import express, {json} from "express";
 import cors from "cors";
 
 const app = express();
-app.use(json());
 app.use(cors());
+app.use(json());
+
 const users=[];
 const tweets=[];
 
-// const user = {
-// 	username: 'bobesponja', 
-// 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-// };
+
 
 // const tweet = {
 // 	username: "bobesponja",
@@ -26,9 +24,36 @@ const tweets=[];
 // ];
 
 app.post("/sign-up",(req,res)=>{
-    const body= req.body;
-    users.push(body);
+    const {username,avatar}= req.body;
+    users.push({
+        username,
+        avatar
+    });
     res.send("OK");
+});
+
+app.post("/tweets",(req,res)=>{
+    const {username,tweet}= req.body;
+    const {avatar} = users.find(user=>user.username==username);
+    tweets.push({
+        username, 
+        avatar,
+        tweet 
+    });
+    res.send("OK");
+});
+
+app.get("/tweets",(req,res)=>{
+    if (tweets.length<=10){
+        res.send(tweets);
+    }
+    else{
+        res.send({
+            username:"Day", 
+            avatar:"https://uploads.spiritfanfiction.com/historias/capas/202107/feliz-aniversario-nami-22601723-030720212110.jpg",
+            tweet:"Tweet chumbado", 
+        });
+    }
 });
 
 app.listen(5000,()=>{
